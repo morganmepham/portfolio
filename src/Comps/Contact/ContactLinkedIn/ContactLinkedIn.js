408630bba8bba4828ca5f0266dea84c7c45632ad
morganmepham <morgancmepham@gmail.com>
2022-11-07 17:57:21 +0000

    Changed LinkedIn Links

--- src/Comps/Contact/ContactLinkedIn/ContactLinkedIn.js
+++ src/Comps/Contact/ContactLinkedIn/ContactLinkedIn.js
@@ -4,7 +4,7 @@ import linkedin_icon from '../../../images/linkedin_icon.png'
 const ContactLinkedIn = () => {
     return (
         <div className="contact-linkedin-div">
-            <a className='contact-link' href='https://www.linkedin.com/in/morgan-mepham-b8445b248/' target= '_blank'>
+            <a className='contact-link' href='https://www.linkedin.com/in/morgan-mepham/' target= '_blank'>
             <img src={linkedin_icon} alt="LinkedIn Icon" className="contact-link-icon" />
             <p className="contact-linkedin-text">LinkedIn</p>
             </a>
--- src/Comps/Home/LinkedIn/LinkedIn.js
+++ src/Comps/Home/LinkedIn/LinkedIn.js
@@ -4,7 +4,7 @@ import linkedin_icon from '../../../images/linkedin_icon.png'
 const LinkedIn = () => {
     return ( 
         <div className="linkedin-div">
-            <a className='linkedin-link' href='https://www.linkedin.com/in/morgan-mepham-b8445b248/' target= '_blank'>
+            <a className='linkedin-link' href='https://www.linkedin.com/in/morgan-mepham/' target= '_blank'>
             <img src={linkedin_icon} alt="LinkedIn Icon" className="linkedin-icon" />
             <p className="linkedin-text">LinkedIn</p>
             </a>
